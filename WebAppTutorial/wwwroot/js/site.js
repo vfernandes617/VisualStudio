06c565a369f4ebf9fb0504aaa16ece7faabea4b2
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-07-06 23:36:24 -0400

    commiting changes to master

--- WebAppTutorial/wwwroot/js/site.js
+++ WebAppTutorial/wwwroot/js/site.js
@@ -11,3 +11,19 @@ function newColor() {
 function oldColor() {
     document.getElementById("hello_world").style.color = "darkolivegreen";
 }
+
+document.addEventListener("DOMContentLoaded", function Mover() {
+    const movingText = document.getElementById("moving-text");
+
+    movingText.addEventListener("mouseover", Mover(),
+
+        const: screenWidth = window.innerWidth,
+        const: screenHeight = window.innerHeight,
+
+        const: newLeft = Math.random() * (screenWidth - movingText.clientWidth),
+        const: newTop = Math.random() * (screenHeight - movingText.clientHeight),
+
+        movingText,style,left = '${newLeft}px',
+        movingText,style,top = '${newTop}px',
+    });
+
