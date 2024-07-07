45e32dd9f1fb0902b09b6e47dfd8ba659c499bb6
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-07-06 22:52:40 -0400

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//Functions to be called onmouseover and to change the color of "Hello World"
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
